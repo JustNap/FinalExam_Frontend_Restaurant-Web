4735783146ab3f25b405929f0a16908accfb622c
JustNap <justinvalen04@gmail.com>
2024-12-05 20:48:55 +0700

    memperbaiki table pada reservasi

--- Restaurant_Web/reservasi/app.js
+++ Restaurant_Web/reservasi/app.js
@@ -1,23 +1,50 @@
 const app = angular.module('reservationApp', []);
 
 app.controller('ReservationController', function ($scope, $timeout) {
-    $scope.tableRows = [
-        ['1A', '1B', '1C', '1D'],
-        ['2A', '2B', '2C', '2D'],
-        ['3A', '3B', '3C', '3D'],
-        ['4A', '4B', '4C', '4D']
-    ];
-
-    $scope.tableStatus = {};
-    $scope.tableRows.flat().forEach(seat => {
-        $scope.tableStatus[seat] = { available: true, reservedUntil: null };
-    });
+    const layouts = {
+        Indoor: [
+            ['1A', '1B', '1C', '1D'],
+            ['2A', '2B', '2C', '2D'],
+            ['3A', '3B', '3C', '3D'],
+            ['4A', '4B', '4C', '4D']
+        ],
+        Outdoor: [
+            ['1A', '1B', '1C', '1D'],
+            ['2A', '2B', '2C'],
+            ['3A', '3B', '3C', '3D'],
+            ['4A', '4B', '4C']
+        ],
+        VIP: [
+            ['1A', '1B', '1C'],
+            ['2A', '2B'],
+            ['3A'],
+            ['4A'],
+            ['5A', '5B'],
+            ['6A', '6B', '6C']
+        ]
+    };
 
     const RESERVATION_TIME = 30 * 60 * 1000;
 
     $scope.reservation = {};
     $scope.reservationDetails = {};
     $scope.isSummaryVisible = false;
+    $scope.tableRows = [];
+    $scope.tableStatus = {};
+
+    $scope.updateTableLayout = function () {
+        const selectedType = $scope.reservation.tableType;
+
+        if (selectedType && layouts[selectedType]) {
+            $scope.tableRows = layouts[selectedType];
+            $scope.tableStatus = {};
+            $scope.tableRows.flat().forEach(seat => {
+                $scope.tableStatus[seat] = { available: true, reservedUntil: null };
+            });
+        } else {
+            $scope.tableRows = [];
+        }
+    };
 
     $scope.isTableAvailable = function (seat) {
         const status = $scope.tableStatus[seat];
@@ -52,13 +79,13 @@ app.controller('ReservationController', function ($scope, $timeout) {
             $scope.tableStatus[seat].available = false;
             $scope.tableStatus[seat].reservedUntil = new Date().getTime() + RESERVATION_TIME;
 
-            $scope.reservationDetails = { 
+            $scope.reservationDetails = {
                 ...$scope.reservation,
                 date: new Date($scope.reservation.date).toLocaleDateString('en-GB'),
                 time: new Date($scope.reservation.time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
             };
 
-            $timeout(function () {
+            $timeout(() => {
                 $scope.tableStatus[seat].available = true;
                 $scope.tableStatus[seat].reservedUntil = null;
             }, RESERVATION_TIME);
@@ -72,5 +99,6 @@ app.controller('ReservationController', function ($scope, $timeout) {
     $scope.resetForm = function () {
         $scope.reservation = {};
         $scope.isSummaryVisible = false;
+        $scope.tableRows = [];
     };
 });
